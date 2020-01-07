ba37086462bf4cadbf4dca9330f6b23375cd6e9f
Mara Just <marajust@mjust03-MacBook-Pro.local>
2020-01-07 14:41:34 +0100

    program the script

---
+++ Assets/MyGame/Scripts/Countdown.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+    public Text CountdownText;
+    public Button AddOneButton;
+
+    public void Start()
+    {
+        CountdownText.text = "0";
+    }
+    public void addOne()
+    {
+        int addNumbers = int.Parse(CountdownText.text) + 1;
+        CountdownText.text = addNumbers.ToString();
+    }
+
+
+
+}
---
+++ Assets/MyGame/Scripts/UserInterface.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInterface : MonoBehaviour
+{
+
+    public Text TextAusgabe;
+    public InputField EingabeInputField;
+    public Button DoItButton;
+
+    public void showValue()
+    {
+        TextAusgabe.text = EingabeInputField.text;
+    }
+}
